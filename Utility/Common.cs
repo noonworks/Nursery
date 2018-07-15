8df0e105911972c686e07ceb45cddc30bd4b0719
noonworks <noonworks@gmail.com>
2018-07-15 19:25:33 +0900

    Split version numbers.

--- Utility/Common.cs
+++ Utility/Common.cs
@@ -1,6 +1,12 @@
 ﻿namespace Nursery.Utility {
 	public class Common {
-		public const string PRODUCT_VERSION = "0.1.0 beta";
+		public const string MAJOR = "0";
+		public const string MINOR = "1";
+		public const string RELEASE = "0";
+		public const string BUILD = "0";
+		public const string POSTFIX = " beta";
+
+		public const string PRODUCT_VERSION = MAJOR + "." + MINOR + "." + RELEASE + "." + BUILD + POSTFIX;
 
 		public const int DISCORD_MAX_MESSAGE_LENGTH = 2000;
 	}
