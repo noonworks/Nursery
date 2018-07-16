fd11b82244bd9f5d2d4d4caee36cfc74ccaeda5e
noonworks <noonworks@gmail.com>
2018-07-16 14:52:41 +0900

    Separate BotState.

--- Nursery/VoiceBot.cs
+++ Nursery/VoiceBot.cs
@@ -10,28 +10,6 @@ using Nursery.Utility;
 using System.Linq;
 
 namespace Nursery {
-	class BotState {
-		public bool Joined { get; set; } = false;
-		public List<ulong> TextChannelIds { get; set; } = new List<ulong>();
-		public ulong VoiceChannelId { get; set; } = 0;
-		public SocketGuild Guild { get; private set; } = null;
-		public string Nickname { get; set; } = "";
-		public ulong[] RoleIds { get; set; } = new ulong[] { };
-
-		public void SetGuild(SocketGuild guild, ulong BotId) {
-			this.Guild = guild;
-			this.Nickname = "";
-			this.RoleIds = new ulong[] { };
-			if (guild != null) {
-				var cu = guild.GetUser(BotId);
-				if (cu != null) {
-					this.Nickname = cu.Nickname;
-					this.RoleIds = cu.Roles.Select(r => r.Id).ToArray();
-				}
-			}
-		}
-	}
-
 	class VoiceBot : IDisposable, IBot {
 		private static VoiceBot instance = null;
 
