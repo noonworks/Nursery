05ec7dbf1e2e49f54707090534cbee914e2be7ca
noonworks <noonworks@gmail.com>
2018-08-13 16:30:24 +0900

    Add translations.

--- locale/build.cmd
+++ locale/build.cmd
@@ -14,6 +14,9 @@ if exist SoundEffectPlugin.edit.pot (
 if exist UserDefinedFilterPlugin.edit.pot (
   msgcat --no-location --output UserDefinedFilterPlugin.pot UserDefinedFilterPlugin.edit.pot
 )
+if exist UserDefinedSchedulerPlugin.edit.pot (
+  msgcat --no-location --output UserDefinedSchedulerPlugin.pot UserDefinedSchedulerPlugin.edit.pot
+)
 
 @REM Build minimized PO file
 if exist Nursery.ja_JP.edit.po (
@@ -28,6 +31,9 @@ if exist SoundEffectPlugin.ja_JP.edit.po (
 if exist UserDefinedFilterPlugin.ja_JP.edit.po (
   msgcat --no-location --output UserDefinedFilterPlugin.ja_JP.po UserDefinedFilterPlugin.ja_JP.edit.po
 )
+if exist UserDefinedSchedulerPlugin.ja_JP.edit.po (
+  msgcat --no-location --output UserDefinedSchedulerPlugin.ja_JP.po UserDefinedSchedulerPlugin.ja_JP.edit.po
+)
 
 @REM Build MO file
 mkdir locale\ja_JP\LC_MESSAGES > NUL 2>&1
@@ -35,6 +41,7 @@ msgfmt --output-file locale\ja_JP\LC_MESSAGES\Nursery.mo Nursery.ja_JP.po
 msgfmt --output-file locale\ja_JP\LC_MESSAGES\BasicPlugins.mo BasicPlugins.ja_JP.po
 msgfmt --output-file locale\ja_JP\LC_MESSAGES\SoundEffectPlugin.mo SoundEffectPlugin.ja_JP.po
 msgfmt --output-file locale\ja_JP\LC_MESSAGES\UserDefinedFilterPlugin.mo UserDefinedFilterPlugin.ja_JP.po
+msgfmt --output-file locale\ja_JP\LC_MESSAGES\UserDefinedSchedulerPlugin.mo UserDefinedSchedulerPlugin.ja_JP.po
 
 @REM Remove files
 del *.mo 2>NUL
