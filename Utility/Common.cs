2e58d3654984468ba1213798a5c5552b397b1c47
noonworks <noonworks@gmail.com>
2019-06-16 20:51:30 +0900

    Increment version number.

--- Utility/Common.cs
+++ Utility/Common.cs
@@ -1,8 +1,8 @@
 ﻿namespace Nursery.Utility {
 	public class Common {
 		public const string MAJOR = "0";
-		public const string MINOR = "4";
-		public const string RELEASE = "2";
+		public const string MINOR = "5";
+		public const string RELEASE = "0";
 		public const string BUILD = "0";
 		public const string POSTFIX = " beta";
 
