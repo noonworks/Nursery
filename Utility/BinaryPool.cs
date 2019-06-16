0fbded0441f24c6173a5f57873b13f2fa360cfad
noonworks <noonworks@gmail.com>
2019-06-16 17:35:28 +0900

    Fix property names in BinaryPool classes.

--- Utility/BinaryPool.cs
+++ Utility/BinaryPool.cs
@@ -53,15 +53,10 @@ namespace Nursery.Utility {
 	}
 
 	public abstract class BinaryFileBase<TData> {
-		protected string _filename;
-		protected bool _isload = false;
-		protected long _size = 0;
-		protected TData _data;
-
-		public string FileName { get => _filename; }
-		public bool IsLoad { get => _isload; }
-		public long Size { get => _size; }
-		public TData Data { get => _data; }
+		public string FileName { get; private set; }
+		public long Size { get; protected set; }
+		public bool IsLoad { get; protected set; } = false;
+		public TData Data { get; protected set; }
 
 		public BinaryFileBase() {}
 
@@ -73,9 +68,9 @@ namespace Nursery.Utility {
 				// TRANSLATORS: Error message. In BinaryPool. {0} is file path.
 				throw new Exception(T._("Binary file [{0}] is not found.", filepath));
 			}
-			this._filename = filepath;
-			var fi = new FileInfo(filepath);
-			this._size = fi.Length;
+			this.FileName = filepath;
+			var fi = new FileInfo(this.FileName);
+			this.Size = fi.Length;
 		}
 		
 		abstract public TData Load();
@@ -84,64 +79,64 @@ namespace Nursery.Utility {
 
 	public class BinaryFile : BinaryFileBase<byte[]> {
 		public BinaryFile() : base() {
-			this._data = new byte[0];
+			this.Data = new byte[0];
 		}
 
 		public override byte[] Load() {
-			if (this.IsLoad) { return this._data; }
+			if (this.IsLoad) { return this.Data; }
 			using (FileStream fs = new FileStream(this.FileName, FileMode.Open, FileAccess.Read)) {
-				if (this._size != fs.Length) { this._size = fs.Length; }
-				this._data = new byte[fs.Length];
-				fs.Read(this._data, 0, this._data.Length);
+				if (this.Size != fs.Length) { this.Size = fs.Length; }
+				this.Data = new byte[fs.Length];
+				fs.Read(this.Data, 0, this.Data.Length);
 			}
-			this._isload = true;
-			return this._data;
+			this.IsLoad = true;
+			return this.Data;
 		}
 
 		public override void UnLoad() {
-			this._data = new byte[0];
-			this._isload = false;
+			this.Data = new byte[0];
+			this.IsLoad = false;
 		}
 	}
 
 	public class UnsafeBinaryFile : BinaryFileBase<SafeBinaryHandle> {
 		public UnsafeBinaryFile() : base() {
-			this._data = default(SafeBinaryHandle);
+			this.Data = default(SafeBinaryHandle);
 		}
 
 		public override SafeBinaryHandle Load() {
-			if (this.IsLoad) { return this._data; }
+			if (this.IsLoad) { return this.Data; }
 			byte[] bytes;
 			using (FileStream fs = new FileStream(this.FileName, FileMode.Open, FileAccess.Read)) {
-				if (this._size != fs.Length) { this._size = fs.Length; }
+				if (this.Size != fs.Length) { this.Size = fs.Length; }
 				bytes = new byte[fs.Length];
 				fs.Read(bytes, 0, bytes.Length);
 			}
-			this._data = SafeBinaryHandle.Create(bytes);
-			this._isload = true;
-			return this._data;
+			this.Data = SafeBinaryHandle.Create(bytes);
+			this.IsLoad = true;
+			return this.Data;
 		}
 
 		public override void UnLoad() {
-			this._data = default(SafeBinaryHandle);
-			this._isload = false;
+			this.Data = default(SafeBinaryHandle);
+			this.IsLoad = false;
 		}
 	}
 
 	public class SafeBinaryHandle : SafeHandleZeroOrMinusOneIsInvalid {
 		private bool released = false;
-		private long size = 0;
 
 		public IntPtr RawIntPtr { get => handle; }
-		public long Size { get => size; }
+		public long Size { get; private set; }
 
 		private SafeBinaryHandle() : base(true) { }
 
 		public static SafeBinaryHandle Create(byte[] bytes) {
-			var instance = new SafeBinaryHandle();
-			instance.size = bytes.Length;
-			// alloc unsafe memory
-			instance.handle = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(byte)) * bytes.Length);
+			var instance = new SafeBinaryHandle {
+				Size = bytes.Length,
+				// alloc unsafe memory
+				handle = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(byte)) * bytes.Length)
+			};
 			// copy bytes to unsafe memory
 			Marshal.Copy(bytes, 0, instance.handle, bytes.Length);
 			return instance;
