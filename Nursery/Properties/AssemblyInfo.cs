ec146d9279ff7a8ced83611961ad104289bd5795
noonworks <noonworks@gmail.com>
2020-11-08 15:22:16 +0900

    Increment versions.

--- Nursery/Properties/AssemblyInfo.cs
+++ Nursery/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(Common.MAJOR + "." + Common.MINOR + ".1.0")]
-[assembly: AssemblyFileVersion(Common.MAJOR + "." + Common.MINOR + ".1.0")]
+[assembly: AssemblyVersion(Common.MAJOR + "." + Common.MINOR + ".0.0")]
+[assembly: AssemblyFileVersion(Common.MAJOR + "." + Common.MINOR + ".0.0")]
 [assembly: AssemblyInformationalVersion(Common.PRODUCT_VERSION)]
